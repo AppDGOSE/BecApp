b0cb50d179727cf284191ab0bdee70882118c307
Emiliano <jemiliano.cabrera@gmail.com>
2014-04-24 11:48:48 -0500

    Agregado el splash screen provisional

---
+++ app/src/main/java/mx/unam/becapp/app/BecAppActivity.java
@@ -0,0 +1,36 @@
+package mx.unam.becapp.app;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+public class BecAppActivity extends Activity {
+
+	private long splashDelay = 2000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bec_app);
+		
+		TimerTask task = new TimerTask() {
+				@Override
+				public void run() {
+					Intent mainIntent;
+					mainIntent = new Intent().setClass(BecAppActivity.this, LoginActivity.class);
+					startActivity(mainIntent);
+
+					/** Destruimos esta activity para prevenit que el usuario retorne aqui
+						presionando el boton Atras.*/						
+					finish();
+
+				}
+			};
+		
+		Timer timer = new Timer();
+		// Comienza el cronometro.
+		timer.schedule(task, splashDelay);
+	} 	
+}
